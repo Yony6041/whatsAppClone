7dddc2172de828f5393cf3f7e51a0b2078bddadf
Yony6041 <berithyjr@gmail.com>
2021-05-06 11:37:39 -0500

    style: I added styling on the chat input

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+const firebase = require('firebase'); 
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBptybgNmRdn_cPbp7vPLXojdetD4Zk22o",
@@ -7,4 +9,15 @@ const firebaseConfig = {
   messagingSenderId: "266558297335",
   appId: "1:266558297335:web:8e897a32ddd4fb9b5eeff2",
   measurementId: "G-CN5Z9JREVK"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth  = firebase.auth();
+const provider =new firebase.auth.GoogleAuthProvider();
+
+// export { auth, provider };
+// export default db;
+module.exports = {
+  auth, provider, db
 };
